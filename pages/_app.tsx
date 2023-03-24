4a59f5e74d5bc6332237544851684911993622b8
RadheDhabas <101125809+RadheDhabas@users.noreply.github.com>
2023-03-24 18:29:22 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
-import { AppProps } from 'next/app'
+import Layout from '@/Components/Layout';
+import { store, persistor } from '../redux/store';
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { SessionProvider } from "next-auth/react"
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+   return (
+      <Provider store={store}>
+         <SessionProvider session={session}>
+            <PersistGate loading={'loading'} persistor={persistor}>
+               <Layout>
+                  <Component {...pageProps} />
+               </Layout>
+            </PersistGate>
+         </SessionProvider> 
+      </Provider>
 
-import '../styles.css'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+   );
 }
 
-export default MyApp
