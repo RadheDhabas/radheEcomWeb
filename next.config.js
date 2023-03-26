cee528b2e9d6050b7a772e9b8962807a804b4d0a
RadheDhabas <101125809+RadheDhabas@users.noreply.github.com>
2023-03-26 23:37:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     stripe_publishable_key: process.env.STRIPE_PUBLISHABLE_KEY,
+     NEXTAUTH_URL: 'https://radhe-ecom-web-hfa8-git-main-radhedhabas.vercel.app',
+        NEXTAUTH_SECRET: 'Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=',
   },
   typescript:{
     ignoreBuildErrors:true,
