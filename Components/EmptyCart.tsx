4fb2a68afecb8600504b980e452fcf056708bc32
RadheDhabas <101125809+RadheDhabas@users.noreply.github.com>
2023-07-21 17:59:29 +0530

    Update EmptyCart.tsx

--- Components/EmptyCart.tsx
+++ Components/EmptyCart.tsx
@@ -2,9 +2,11 @@ import Link from 'next/link'
 import React from 'react'
 import style from '../styles/Cart.module.css';
 function EmptyCart() {
+  let x = process.env.STRIPE_SECRET_KEY;
   return (
     <div className={style.empty_cart}>
       <p>{process.env.mail}</p>
+       <p>{x}</p>
      <p className={style.empty_cart_heading}>
      Your shopping cart is Empty!
      </p>
